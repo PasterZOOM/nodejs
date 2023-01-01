0cfca554e2a91059c7247c0b9e6714704c644ba3
Ilya Pasternak <pasterzoom@gmail.com>
2023-01-01 16:04:33 +0300

    middlewares, validation

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, {NextFunction, Request, Response} from 'express'
 import bodyParser from 'body-parser'
 import {productsRouter} from "./routs/products-router";
 import {addressesRouter} from "./routs/addresses-router";
@@ -8,6 +8,15 @@ const port = process.env.PORT || 5000
 
 const parserMiddleware = bodyParser({})
 app.use(parserMiddleware)
+
+const authGuardMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    if (req.query.token === '123') {
+        next()
+    } else {
+        res.send(401)
+    }
+}
+app.use(authGuardMiddleware)
 
 app.use('/products', productsRouter)
 app.use('/addresses', addressesRouter)
--- src/routs/products-router.ts
+++ src/routs/products-router.ts
@@ -1,41 +1,59 @@
 import {Request, Response, Router} from 'express'
 import {productsRepository} from "../repositories/products-repository";
+import {body, validationResult} from "express-validator";
+import {inputValidationMiddlewares} from "../middlewares/input-validation-middlewares";
 
 export const productsRouter = Router({})
+const titleValidation = body('title').trim().isLength({
+    min: 3,
+    max: 255
+}).withMessage('Title length must be from 3 to 255 symbols')
 
 productsRouter.get('/', (req: Request, res: Response) => {
     const fontProducts = productsRepository.findProducts(req.query.title?.toString())
 
     res.send(fontProducts)
 })
-
 productsRouter.get('/:id', (req: Request, res: Response) => {
     const product = productsRepository.findProductById(+req.params.id)
 
-    if (!product) return res.send(404)
+    if (!product) res.send(404)
     res.send(product)
 })
+productsRouter.post('/',
+    titleValidation,
+    inputValidationMiddlewares,
+    (req: Request, res: Response) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.status(400).json({errors: errors.array()});
+        }
 
-productsRouter.post('/', (req: Request, res: Response) => {
-    const newProduct = productsRepository.createProduct(req.body.title)
+        const newProduct = productsRepository.createProduct(req.body.title)
 
-    res.status(201).send(newProduct)
-})
+        res.status(201).send(newProduct)
+    })
+productsRouter.put('/:id',
+    titleValidation,
+    inputValidationMiddlewares,
+    (req: Request, res: Response) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.status(400).json({errors: errors.array()});
+        }
 
-productsRouter.put('/:id', (req: Request, res: Response) => {
-    const id = +req.params.id;
-    const isUpdated = productsRepository.updateProduct(id, req.body.title)
+        const id = +req.params.id;
+        const isUpdated = productsRepository.updateProduct(id, req.body.title)
 
-    if (isUpdated) {
-        const product = productsRepository.findProductById(id)
-        return res.send(product)
-    }
-    res.send(404)
-})
-
+        if (isUpdated) {
+            const product = productsRepository.findProductById(id)
+            res.send(product)
+        }
+        res.send(404)
+    })
 productsRouter.delete('/:id', (req: Request, res: Response) => {
     const isDeleted = productsRepository.deleteProduct(+req.params.id)
 
-    if (!isDeleted) return res.send(404)
+    if (!isDeleted) res.send(404)
     res.send(204)
 })
